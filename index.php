b73cffeddd05d68ea8bee5fc48e90d814154c740
Elites <luckyboy.hqh@gmail.com>
2012-08-24 06:56:07 +0700

    Khoi tao project

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Jquery</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+    <link rel="stylesheet" href="css/stylesheet.css" type="text/css" />
+    <script type="text/javascript" src="jquery-directory/file-jquery.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+
+        });
+    </script>
+
+</head>
+<body>
+    <div id="wraper">
+        <div id="banner"></div>
+        <div id="content">
+            <div class="column">
+                <ul class="nav">
+                    <li><a href="#">Home page</a></li>
+                    <li><a href="#">Ebook</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Forum</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="main">
+
+                <h1>Jquery learning</h1>
+                <p>Gio moi phat hien ra diem hay cua jquery</p>
+                <h3>Jquery!!!</h3>
+                <p>Tuyet qua</p>
+
+            </div>
+        </div>
+        <div id="footer"></div>
+    </div>
+
+</body>
+</html>
